467d52dbb232686fd5b2fa9a7e52d90a179d7805
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-02 06:35:47 +0200

    Keep app extension registry generic

--- src/appExtensionRegistry.ts
+++ src/appExtensionRegistry.ts
@@ -1,10 +1,3 @@
-import { ChessBoard, OpeningBook } from '@ankhorage/zora-chess';
-import { TabletopTable } from '@ankhorage/zora-tabletop';
-
 import type { ComponentRegistry } from './componentRegistry';
 
-export const APP_EXTENSION_COMPONENT_REGISTRY: ComponentRegistry = {
-  ChessBoard,
-  OpeningBook,
-  TabletopTable,
-};
+export const APP_EXTENSION_COMPONENT_REGISTRY: ComponentRegistry = {};
